3abef8f02e15a2a1dc9725fc170d95600c9dd65d
ozgenur00 <ozgenurcatal00@gmail.com>
2025-02-07 13:03:03 -0500

    Database Changed

--- config.py
+++ config.py
@@ -1,20 +1,25 @@
 import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'default_secret_key')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
 
 class DevelopmentConfig(Config):
     FLASK_ENV = 'development'
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql://dfumjnnp:Bd4UsUlodu6KrHnaZj99_NRU7jFWiSZU@kala.db.elephantsql.com/dfumjnnp'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql:///financemanager')
 
 class ProductionConfig(Config):
     FLASK_ENV = 'production'
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = 'postgresql://dfumjnnp:Bd4UsUlodu6KrHnaZj99_NRU7jFWiSZU@kala.db.elephantsql.com/dfumjnnp'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
 
-class TestingConfig(Config):
+
+class TestingConfig(Config): 
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'  # In-memory SQLite database for testing
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:' 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
